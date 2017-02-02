44ac428bbe32da76d674ce849756e1736e47f32e
Chris Thomas <cthomas@aos.biz>
2017-02-02 11:36:07 -0600

    Added Number Complement solution Added Number Complement unit tests

---
+++ LeetCodeSolutions.Tests/NumberComplement476Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeSolutions.Tests
+{
+    public class NumberComplement476Tests
+    {
+        private readonly NumberComplement476 _numbercomplement;
+
+        public NumberComplement476Tests()
+        {
+            _numbercomplement = new NumberComplement476();
+        }
+
+        [Theory]
+        [InlineData(5, 2)]
+        [InlineData(1, 0)]
+        public void TwoSum_TwoOrMoreElementsWithSolution_ReturnsCorrect(int num, int expected)
+        {
+            int result = _numbercomplement.FindComplement(num);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCodeSolutions/NumberComplement476.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    /// <summary>
+    /// Solutions for https://leetcode.com/problems/number-complement/
+    /// </summary>
+    public class NumberComplement476
+    {
+        /// <summary>
+        /// Basic implementation using string manipulation [ACCEPTED]
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int FindComplement(int num)
+        {
+            var binaryStr = Convert.ToString(num, 2);
+            var arr = binaryStr.ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '0')
+                    arr[i] = '1';
+                else if (arr[i] == '1')
+                    arr[i] = '0';
+            }
+            binaryStr = new string(arr);
+            return Convert.ToInt32(new string(arr), 2);
+        }
+    }
+}
