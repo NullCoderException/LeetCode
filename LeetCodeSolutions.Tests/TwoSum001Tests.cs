362471fe3e7a685b6f094931f6a8a30bf32f7a30
Chris Thomas <cthomas@aos.biz>
2017-02-02 11:09:57 -0600

    Reorganized solution into folders Added test project using XUnit and tests for Two Sum.

---
+++ LeetCodeSolutions.Tests/TwoSum001Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace LeetCodeSolutions.Tests
+{
+    public class TwoSum001Tests
+    {
+        private readonly TwoSum001 _twosum;
+        public TwoSum001Tests()
+        {
+            _twosum = new TwoSum001();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        public void TwoSum_TwoOrMoreElementsWithSolution_ReturnsCorrect(int[] nums, int target, int[] expected)
+        {
+            int[] result = _twosum.TwoSum(nums, target);
+            Assert.Equal(expected, result);
+        }
+    }
+}
