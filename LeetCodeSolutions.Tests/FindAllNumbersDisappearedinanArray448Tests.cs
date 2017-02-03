7d791ddbaa645574297984bc5178106c97f51ade
Chris Thomas <cthomas@aos.biz>
2017-02-02 19:52:05 -0600

    Added solution for FindAllNumbersDisappearedinanArray Added test for FindAllNumbersDisappearedinanArray

---
+++ LeetCodeSolutions.Tests/FindAllNumbersDisappearedinanArray448Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeSolutions.Tests
+{
+    public class FindAllNumbersDisappearedinanArray448Tests
+    {
+        private readonly FindAllNumbersDisappearedinanArray448 _disappeared;
+        public FindAllNumbersDisappearedinanArray448Tests()
+        {
+            _disappeared = new FindAllNumbersDisappearedinanArray448();   
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4, 3, 2, 7, 8, 2, 3, 1 }, new int[] { 5, 6 })]
+        public void FindDisappearedNumbers_SmallArray_CorrectResult(int[] nums, int[] expected)
+        {
+            var result = _disappeared.FindDisappearedNumbers(nums);
+            Assert.Equal(result, expected.ToList());
+        }
+    }
+}
---
+++ LeetCodeSolutions/FindAllNumbersDisappearedinanArray448.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class FindAllNumbersDisappearedinanArray448
+    {
+        /// <summary>
+        /// Solution using Linq
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            return Enumerable.Range(1, nums.Length).Except(nums).ToList();
+        }
+    }
+}
