8c6fce8636e7658fa8ce23f1086bb5eb2f39c1bc
Chris Thomas <cthomas@aos.biz>
2017-02-03 11:22:07 -0600

    Added FizzBuzz solution Added FizzBuzz unit tests

---
+++ LeetCodeSolutions.Tests/FizzBuzz412Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeSolutions.Tests
+{
+    public class FizzBuzz412Tests
+    {
+        private readonly FizzBuzz412 _fizzbuzz;
+        public FizzBuzz412Tests()
+        {
+            _fizzbuzz = new FizzBuzz412();
+        }
+
+        [Theory]
+        [InlineData(1, new string[] { "1" })]
+        [InlineData(3, new string[] { "1", "2", "Fizz" })]
+        [InlineData(5, new string[] { "1", "2", "Fizz", "4", "Buzz" })]
+        [InlineData(15, new string[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" })]
+        public void FizzBuzz_SmallInteger_ExpectedOutput(int num, string[] expected)
+        {
+            string[] result = _fizzbuzz.FizzBuzz(num).ToArray();
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCodeSolutions/FizzBuzz412.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class FizzBuzz412
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            return FizzBuzzSimpleTraversal(n);
+        }
+
+        /// <summary>
+        /// Basic approach using array traversal and if statements 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public IList<string> FizzBuzzSimpleTraversal(int n)
+        {
+            List<string> result = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+            }
+            return result;
+        }
+    }
+}
